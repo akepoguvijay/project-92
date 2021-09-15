aaaf8cd25d08ec9316c3af12c90579e844a18fce
akepoguvijay <85406979+akepoguvijay@users.noreply.github.com>
2021-09-15 19:47:02 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+function addUser()
+{
+    player1_name = document.getElementById("player_1_name").value;
+    player2_name = document.getElementById("player_2_name").value;
+
+    localStorage.setItem("Player1 Name",player1_name);
+    localStorage.setItem("Player2 Name",player2_name);
+
+    window.location= "game_page.html";
+
+}
